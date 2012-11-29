0172e884163cee33ee4b567fd368861606b9ddb4
Etienne <etienne.valette@gmail.com>
2012-11-29 16:18:09 +0100

    with calendar

---
+++ src/edu/chalmers/project/DatePickerFragment.java
@@ -0,0 +1,35 @@
+package edu.chalmers.project;
+
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+	public EditText activity_edittext;
+
+	public DatePickerFragment(EditText edit_text) {
+	    activity_edittext = edit_text;
+	}
+	
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	// Use the current date as the default date in the picker
+	final Calendar c = Calendar.getInstance();
+	int year = c.get(Calendar.YEAR);
+	int month = c.get(Calendar.MONTH);
+	int day = c.get(Calendar.DAY_OF_MONTH);
+	
+	// Create a new instance of DatePickerDialog and return it
+	return new DatePickerDialog(getActivity(), this, year, month, day);
+	}
+	
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		activity_edittext.setText(String.valueOf(month + 1 ) + "/" + String.valueOf(day) + "/" + String.valueOf(year));
+	}
+}
--- src/edu/chalmers/project/NewAccountActivity.java
+++ src/edu/chalmers/project/NewAccountActivity.java
@@ -1,24 +1,30 @@
 package edu.chalmers.project;
 
+
 import android.os.Build;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class NewAccountActivity extends Activity {
+public class NewAccountActivity extends FragmentActivity {
 
+	EditText dateOfBirth;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_account);
         
-        EditText dateOfBirth = (EditText)findViewById(R.id.editTextDateOfBirth);
+        dateOfBirth = (EditText)findViewById(R.id.editTextDateOfBirth);
         dateOfBirth.setKeyListener(null);       
     }
     
@@ -31,6 +37,23 @@ public class NewAccountActivity extends Activity {
     		actionBar.setDisplayShowTitleEnabled(true);
     	}
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case android.R.id.home:
+			onBackPressed();
+			return true;
+    	case R.id.menu_create:
+    		Intent intent = new Intent(this, FirstScreenActivity.class);
+    		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
+    		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    		startActivity(intent);
+    		return true;    
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
 
     /**
@@ -45,5 +68,11 @@ public class NewAccountActivity extends Activity {
     public void showCalendar(View view){
     	Intent intent = new Intent(this, PopupCalendarActivity.class);
     	startActivity(intent);
+    }
+    
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment(this.dateOfBirth);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+        
     }
 }
