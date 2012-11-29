e117974217e562264e06d898bf41fe47c790b530
Etienne <etienne.valette@gmail.com>
2012-11-29 14:53:14 +0100

    with popup date of birth

--- src/edu/chalmers/project/NewAccountActivity.java
+++ src/edu/chalmers/project/NewAccountActivity.java
@@ -1,17 +1,36 @@
 package edu.chalmers.project;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewAccountActivity extends Activity {
 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_account);
+        
+        EditText dateOfBirth = (EditText)findViewById(R.id.editTextDateOfBirth);
+        dateOfBirth.setKeyListener(null);       
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.create_event_list, menu);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+    		ActionBar actionBar = getActionBar();
+    		actionBar.setDisplayHomeAsUpEnabled(true);
+    		actionBar.setDisplayShowTitleEnabled(true);
+    	}
+        return true;
     }
 
     /**
@@ -20,6 +39,11 @@ public class NewAccountActivity extends Activity {
      */
     public void confirmAccount(View view){
     	Intent intent = new Intent(this, LoginActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void showCalendar(View view){
+    	Intent intent = new Intent(this, PopupCalendarActivity.class);
     	startActivity(intent);
     }
 }
