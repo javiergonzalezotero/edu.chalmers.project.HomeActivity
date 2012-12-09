335b5b25df824ca75508452c78ff0682c25515a6
Etienne <etienne.valette@gmail.com>
2012-12-09 18:30:20 +0100

    d

---
+++ src/edu/chalmers/project/ChangeResultActivity.java
@@ -0,0 +1,128 @@
+package edu.chalmers.project;
+
+import java.util.ArrayList;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import android.widget.Toast;
+
+import edu.chalmers.project.data.MatchDBAdapter;
+
+import edu.chalmers.project.data.MatchPlayedDBAdapter;
+import edu.chalmers.project.data.Player;
+
+
+public class ChangeResultActivity extends Activity {
+
+	public static final int HOST = 1;
+	public static final int GUEST = 2;
+	private int idMatch;
+	private String playerUsername;
+	private ArrayList<Player> hostTeam = new ArrayList<Player>();
+	private ArrayList<Player> guestTeam = new ArrayList<Player>();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.change_result);		
+		
+		this.hostTeam = new ArrayList<Player>();
+    	this.guestTeam = new ArrayList<Player>();
+            
+    	Bundle b = getIntent().getExtras();
+    	this.playerUsername = b.getString("username");
+    	this.idMatch = b.getInt("position_id_match");
+    	MatchDBAdapter adapter = new MatchDBAdapter(this);
+    	adapter.open();
+    	Cursor cursor = adapter.getMatch(this.idMatch);
+    	this.setTitle("Change result of " + cursor.getString(3));
+    	cursor.close();
+    	adapter.close();
+ 
+    	MatchPlayedDBAdapter matchPlayedAdapter = new MatchPlayedDBAdapter(this);
+        matchPlayedAdapter.open();
+   
+        hostTeam = matchPlayedAdapter.getTeam(HOST, idMatch);
+        guestTeam = matchPlayedAdapter.getTeam(GUEST, idMatch);
+        matchPlayedAdapter.close();
+    	ListView lvListHost = (ListView) findViewById(R.id.listViewHostTeamChangeResult);
+    	ListView lvListGuest = (ListView) findViewById(R.id.listViewGuestTeamChangeResult);
+        lvListHost.setAdapter(new ArrayAdapter<Player>(this, android.R.layout.simple_list_item_1, hostTeam));
+        lvListGuest.setAdapter(new ArrayAdapter<Player>(this, android.R.layout.simple_list_item_1,guestTeam));
+        
+        
+
+        lvListHost.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id){	
+				String playerUsernameSelected = hostTeam.get(position).getUsername();
+				//Toast.makeText(view.getContext(), "username " + playerUsernameSelected, Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent(view.getContext(), PopupChangeResultActivity.class);
+				intent.putExtra("idMatch", idMatch);
+				intent.putExtra("playerUsernameSelected", playerUsernameSelected);
+				intent.putExtra("currentUsernameLogin", playerUsername);
+				intent.putExtra("nextGoal", 0);
+				startActivity(intent);
+
+			}
+		});
+        
+        lvListGuest.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id){	
+				String playerUsernameSelected = guestTeam.get(position).getUsername();
+				//Toast.makeText(view.getContext(), "username " + playerUsernameSelected, Toast.LENGTH_SHORT).show();
+				Intent intent = new Intent(view.getContext(), PopupChangeResultActivity.class);
+				intent.putExtra("idMatch", idMatch);
+				intent.putExtra("playerUsernameSelected", playerUsernameSelected);
+				intent.putExtra("currentUsernameLogin", playerUsername);
+				intent.putExtra("nextGoal", 0);
+				startActivity(intent);
+
+			}
+		});
+		
+		
+	}
+	
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+    		ActionBar actionBar = getActionBar();
+    		actionBar.setDisplayHomeAsUpEnabled(true);
+    		actionBar.setDisplayShowTitleEnabled(true);
+    	}
+        return true;
+    }
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	
+    
+  
+    
+}
+
